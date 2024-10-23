722b128ccc76fb5983b33281e5604b66ded65240
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2024-10-22 20:26:43 -0600

    ventas

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -148,6 +148,10 @@ MEDIA_URL = "/public/"
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Aquí defines dónde están los archivos estáticos en tu proyecto
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
