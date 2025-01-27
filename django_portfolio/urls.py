09f5444360c9d9df27c6b38469a727da647286b7
oswaldo-herrera <oswaldo.herrera.mx@gmail.com>
2025-01-27 12:40:59 -0600

    login

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from django.conf.urls.static import static
@@ -23,6 +24,8 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('',  views.home,name='home'),
     path('blog/', include('blog.urls'))
 ]
